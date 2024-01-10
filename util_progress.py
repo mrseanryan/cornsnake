d4585602ac0665986d009c330e0bbb4cb4f4a512
Sean Ryan <sean.ryan@mendix.com>
2024-01-10 17:27:34 +0100

    Add util progress bar

---
+++ util_progress.py
@@ -0,0 +1,12 @@
+import sys
+
+def progress(count, total, status='', bar_len=60):
+    filled_len = int(round(bar_len * count / float(total)))
+
+    percents = round(100.0 * count / float(total), 1)
+    bar = '=' * filled_len + '-' * (bar_len - filled_len)
+
+    fmt = '[%s] %s%s ...%s' % (bar, percents, '%', status)
+    print('\b' * len(fmt), end='')  # clears the line
+    sys.stdout.write(fmt)
+    sys.stdout.flush()
