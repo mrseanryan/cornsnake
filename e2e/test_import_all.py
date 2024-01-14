fc45dc2be99f6eb0e770e649e5c095c81cdf35c9
Sean Ryan <sean.ryan@mendix.com>
2024-01-14 15:27:03 +0100

    Improve e2e

--- e2e/test_import_all.py
+++ e2e/test_import_all.py
@@ -8,6 +8,15 @@ cornsnake.util_print.print_warning("a warning")
 print('util_print.print_error("an error")')
 cornsnake.util_print.print_error("an error")
 
+print("== util_progress ==")
+total = 1000
+count = 10
+cornsnake.util_progress.progress(count, total)
+cornsnake.util_wait.wait_seconds(1)
+cornsnake.util_progress.progress(50, total)
+cornsnake.util_wait.wait_seconds(1)
+cornsnake.util_progress.complete()
+
 print("== util_string ==")
 print('util_string.is_empty("")', cornsnake.util_string.is_empty(""))
 print('util_string.is_empty(" ")', cornsnake.util_string.is_empty(" "))
--- e2e/test_import_one.py
+++ e2e/test_import_one.py
@@ -1,4 +1,4 @@
-from cornsnake import util_print, util_string, __version__
+from cornsnake import util_print, util_progress, util_string, util_wait, __version__
 
 print(f"Using cornsnake [{__version__}] - 'from import X'")
 
@@ -7,6 +7,15 @@ print('util_print.print_warning("a warning")')
 util_print.print_warning("a warning")
 print('util_print.print_error("an error")')
 util_print.print_error("an error")
+
+print("== util_progress ==")
+total = 1000
+count = 10
+util_progress.progress(count, total)
+util_wait.wait_seconds(1)
+util_progress.progress(50, total)
+util_wait.wait_seconds(1)
+util_progress.complete()
 
 print("== util_string ==")
 print('util_string.is_empty("")', util_string.is_empty(""))
