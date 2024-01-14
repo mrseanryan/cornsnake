c9d777bd5bbe19fe53ccb7dd50e4568424d26aec
Sean Ryan <sean.ryan@mendix.com>
2024-01-14 15:27:22 +0100

    Read lines - strip left as well as right

--- src/cornsnake/util_file.py
+++ src/cornsnake/util_file.py
@@ -10,7 +10,7 @@ def copy_file(from_path, to_path):
 def read_lines_from_file(filepath):
     lines = []
     with open(filepath, encoding='utf-8') as file:
-        lines = [line.rstrip() for line in file]
+        lines = [line.strip() for line in file]
     return lines
 
 def read_text_from_file(filepath):
