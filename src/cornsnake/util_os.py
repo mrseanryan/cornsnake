289829898f596d1546901ba513cd256f32125b16
Sean Ryan <sean.ryan@mendix.com>
2024-01-25 15:05:03 +0100

    Add Windows registry reading

--- src/cornsnake/util_os.py
+++ src/cornsnake/util_os.py
@@ -1,4 +1,27 @@
 import os
+import winreg
+
+from . import util_log
+
+logger = util_log.getLogger(__name__)
 
 def is_windows():
     return os.name == 'nt'
+
+def get_registry_key(top_key, reg_path, name):
+    try:
+        registry_key = winreg.OpenKey(top_key, reg_path, 0,
+                                       winreg.KEY_READ)
+        value, _regtype = winreg.QueryValueEx(registry_key, name)
+        winreg.CloseKey(registry_key)
+        return value
+    except WindowsError:
+        return None
+
+def is_windows_max_path_setting_on():
+    path = 'SYSTEM\CurrentControlSet\Control\FileSystem'  # HKEY_LOCAL_MACHINE is implied
+    key = 'LongPathsEnabled'
+    value = get_registry_key(winreg.HKEY_LOCAL_MACHINE, path, key)
+    if value is None:
+        return False
+    return str(value) == '1'
