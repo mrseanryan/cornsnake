b04d81430509ecb0b1822294e64151722b120d50
Sean Ryan <sean.ryan@mendix.com>
2024-04-08 11:46:09 +0200

    Add replace_keep_case()

--- cornsnake/util_string.py
+++ cornsnake/util_string.py
@@ -1,3 +1,5 @@
+import re
+
 """
 Check if a text string is empty. The `is_empty` function checks if a text string is None or contains only whitespace or a hyphen.
 
@@ -19,3 +21,23 @@ def is_empty(text):
         return True
     stripped = text.strip()
     return stripped == "" or stripped == "-"
+
+
+def replace_keep_case(word__for_regex, replacement, text):
+    """
+    Replace ocurrences of 'word__for_regex' in 'text', with 'replacement', trying to maintain the same casing.
+
+    - supports lower, titla and upper casing
+    """
+
+    def func(match):
+        g = match.group()
+        if g.islower():
+            return replacement.lower()
+        if g.istitle():
+            return replacement.title()
+        if g.isupper():
+            return replacement.upper()
+        return replacement
+
+    return re.sub(word__for_regex, func, text, flags=re.I)
