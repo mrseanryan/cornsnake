f19151abdfee2262f5d4195fd44e6ba3ce5e478c
Sean Ryan <str_ie@yahoo.co.uk>
2024-05-24 13:21:33 +0200

    Add util_network.get_file(). Alter post_request() to raise error if failed, not just return False.

--- cornsnake/util_network.py
+++ cornsnake/util_network.py
@@ -1,17 +1,32 @@
 """
-Making a POST request using the urllib library.
+Download files or make a POST request without adding extra libraries.
 
 [Documentation](http://docs.mrseanryan.cornsnake.s3-website-eu-west-1.amazonaws.com/cornsnake/util_network.html)
 """
 
+import requests
 import urllib.request
 
 from . import util_log
 
 logger = util_log.getLogger(__name__)
 
+def get_file(url, local_path_to_write_file, timeout=60):
+    """
+    Function to download a file from a URL to a local file path, via a HTTP GET request.
 
-def post_request(api_url, headers, timeout):
+    Args:
+    url (str): The URL to download from, via a GET request.
+    timeout (int): The timeout for the request in seconds.
+    """
+    response = requests.get(url, timeout=timeout)
+    if response.status_code == 200:
+        with open(local_path_to_write_file, "wb") as file:
+            file.write(response.content)
+    else:
+        raise RuntimeError(f"File download failed. HTTP status code: {response.status_code}")
+
+def post_request(api_url, headers, timeout=60):
     """
     Function to make a POST request to a specified API URL.
 
@@ -21,7 +36,7 @@ def post_request(api_url, headers, timeout):
     timeout (int): The timeout for the request in seconds.
 
     Returns:
-    bool: True if the POST request is successful (status code 200), False otherwise.
+    bool: True if the POST request is successful (status code 200). Otherwise raises RuntimeError.
     """
     req = urllib.request.Request(
         url=api_url,
@@ -34,5 +49,4 @@ def post_request(api_url, headers, timeout):
     with urllib.request.urlopen(req, timeout=timeout) as response:
         if response.status == 200:
             return True
-        print(f"POST failed [{response.status}]")
-        return False
+        raise RuntimeError(f"POST failed. HTTP status code: [{response.status}]")
