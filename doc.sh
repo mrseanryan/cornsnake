b8778c5dd2bbab821ac301160f496297a8b29f11
mrseanryan <str_ie@yahoo.co.uk>
2026-02-14 22:50:52 +0100

    fix docs script

--- doc.sh
+++ doc.sh
@@ -5,7 +5,7 @@ rm -rf docs
 mkdir docs
 
 # Outputs lot of errors, but DOES generate docs:
-pdoc ./cornsnake !cornsnake.setup --output-directory  docs
+uv run python -m pdoc ./cornsnake !cornsnake.setup --output-directory  docs
 
 echo "Outputs lot of errors, but DOES generate docs"
 echo "./docs"
